a209090e06f202d471e047a52a662c6f27eaadc5
Marsha <56554597+marsha71@users.noreply.github.com>
2024-11-09 14:40:48 -0500

    zdfg

--- VCF_Lab_Install/vcf-nested-hosts-only.ps1
+++ VCF_Lab_Install/vcf-nested-hosts-only.ps1
@@ -17,7 +17,11 @@ $NestedESXiApplianceOVA = "/Users/mc006068broadcom.net/Downloads/Nested_ESXi8.0u
 
 # Nested ESXi VMs for Management Domain
 $NestedESXiHostnameToIPsForManagementDomain = @{
-    "VCF-DDC-ESX205"   = "10.0.0.205"
+    "VCF-DDC-ESX206"   = "10.0.0.206"
+        "VCF-DDC-ESX207"   = "10.0.0.207"
+            "VCF-DDC-ESX208"   = "10.0.0.208"
+
+
 } # End Nested Names
 
 #"VCF-DDC-ESX186"   = "192.168.4.186"
